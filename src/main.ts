cf94308d4dba01bd134d7ebcc4de1faa0b885fcf
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-02-22 22:31:40 +1100

    Implement sliding log

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,52 @@
 import { Request, Response } from "express";
 import { createClient } from "redis";
 
-import { handleRootRequest, AppRequest, AppResponse } from "./app";
 import { unexpectedError } from "./error";
 
-const redisClient = createClient();
-redisClient.connect();
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
+const rc = createClient();
+rc.connect();
+rc.on("error", (err) => console.log("Redis Client Error", err));
 
 require("dotenv").config();
 
 const app = require("express")();
 const port = 3000;
 
+// using sliding log
+async function rateLimit(ip: string): Promise<boolean> {
+    const pastTime = new Date();
+    pastTime.setHours(pastTime.getHours() - 1); // TODO: make time configrable?
+    await rc.zRemRangeByScore(ip, "-inf", pastTime.getTime());
+
+    const curTime: number = Date.now();
+    await rc.zAdd(ip, { score: curTime, value: "" });
+
+    const count = await rc.zCount(ip, "-inf", "inf");
+    if (count < 100) return true; // TODO: make rate configurable
+    return false;
+}
+
 app.get("/", async (req: Request, res: Response): Promise<void> => {
     try {
-        const tmpreq: AppRequest = { ip: req.ip ? req.ip : "" };
-        const ans: AppResponse = handleRootRequest(tmpreq);
-        res.status(ans.status).send(ans.obj);
+        if (!req.ip) {
+            res.status(400).send({ message: "bad request" });
+            return;
+        }
+
+        const isLimited: boolean = await rateLimit(req.ip);
+        if (isLimited) {
+            res.status(429).send({ message: "rate limit exceeded" });
+            return;
+        }
+
+        res.status(200).send({ message: "success" });
     } catch (error: unknown) {
         unexpectedError(error as Error);
     }
 });
 
 app.get("/health", async (req: Request, res: Response): Promise<void> => {
-    try {
-        console.log(await redisClient.set("name", "ryan"));
-        console.log(await redisClient.get("name"));
-        res.status(200).send("health\n");
-    } catch (error: unknown) {
-        unexpectedError(error as Error);
-    }
+    res.status(200).send("health\n");
 });
 
 app.listen(port, () => {
